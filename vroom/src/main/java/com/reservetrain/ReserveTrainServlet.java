316ea85de88af903108852da77f5495e9d3eeb07
ham0612 <hyemangi@naver.com>
2022-10-24 20:22:03 +0900

    안혜민 - 서블릿 재수정

--- vroom/src/main/java/com/reservetrain/ReserveTrainServlet.java
+++ vroom/src/main/java/com/reservetrain/ReserveTrainServlet.java
@@ -2,9 +2,6 @@ package com.reservetrain;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.List;
 
 import javax.servlet.ServletException;
